0782f44a44570505e2a064d5e202de879180a4d8
baha ferchichi <ferchichibaha7@gmail.com>
2024-07-11 13:30:02 +0100

    test orinign

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
+const allowedOrigins = ['https://bahaferchichi.com']; // Replace with your allowed domain(s)
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    const { name = 'World' } = req.query;
+    return res.json({
+      message: `Hello ${name}!`,
+    });
+  } else {
+    res.status(403).json({ message: 'Access denied' });
+  }
 }
